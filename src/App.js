70f9c36abd12ed963e542195af5ebcbe90f2bd23
MoAqsam <MoChainz@localhost.com>
2019-01-12 08:59:02 -0500

    tested api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Axios from "axios";
 
 class App extends Component {
+  constructor(props){
+    super(props);
+  }
+  componentWillMount(){
+    Axios.get("https://secure.toronto.ca/cc_sr_v1/data/swm_waste_wizard_APR?limit=1000").then(data=>{
+      console.log(data);
+    }).catch(err=>{
+      console.log(err);
+    });
+  }
+  componentDidMount(){
+    console.log(this.state);
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="header">
+          <h1>Toronto Waste Lookup</h1>
+        </div>
+        <div className="Search-Area">
+          <input type="text" placeholder="Enter here"></input> <img alt="search"></img>
+        </div>
       </div>
     );
   }
