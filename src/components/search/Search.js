7e943cb3b67bc3f1c4240fb8d39ca8c46a88370f
MoAqsam <MoChainz@localhost.com>
2019-01-12 21:19:31 -0500

    removed unused variables

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,4 +1,4 @@
-import React, {Component,Fragment} from 'react';
+import React, {Component} from 'react';
 //img
 import greyStar from "../../img/greyStar.PNG";
 import greenStar from "../../img/greenStar.PNG";
